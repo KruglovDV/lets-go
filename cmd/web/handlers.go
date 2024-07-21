5d928ad1ef6641c4864affd8d34012a28686e6d5
DmitryK <kruglov081289@gmail.com>
2024-07-21 19:50:06 +0300

    feat: add http error helpers for application

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -22,21 +22,21 @@ func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles(templateFiles...)
 	if err != nil {
 		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal error", http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
 func (app *Application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -46,7 +46,7 @@ func (app *Application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *Application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
