fb27eeacee9c387082829d6906aaaed3521ca082
DmitryK <kruglov081289@gmail.com>
2024-07-21 16:13:48 +0300

    feat: add custome loggers for info and errors

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -21,8 +25,14 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Strarting server on %v\n", *addr)
+	srv := http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	infoLog.Printf("Strarting server on %v\n", *addr)
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
